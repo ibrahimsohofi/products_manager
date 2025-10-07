63a44d80b447c223684be4ececec3cb34d8b55ce
ibrahimsohofi <brahimsohofi9@gmail.com>
2025-10-07 10:01:01 +0000

    first commit

--- jamalbrico/server/config/database.js
+++ jamalbrico/server/config/database.js
@@ -9,7 +9,7 @@ const dbConfig = {
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'jamalbrico',
+  database: process.env.DB_NAME || 'bricojamal',
   multipleStatements: true,
   charset: 'utf8mb4'
 };
